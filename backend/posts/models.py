654981d51198c8d271d799b424d59b58a43bd327
enesrptr <enesmalikyildiz8@gmail.com>
2025-05-15 17:04:59 +0300

    models and serializers

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Posts(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField
+    content = models.TextField(null=True, blank=True)
     author = models.CharField(max_length = 100)
     date = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
